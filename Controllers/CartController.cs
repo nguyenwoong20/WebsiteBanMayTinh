836274dc7c8dda7caa136ff10dbb739976ad2a0e
Nguyen Le Hoan Hao <nguyenlehoanhao2004@gmail.com>
2025-06-23 23:28:39 +0700

    HotFix Cart_Checkout
    Đặt hàng nhưng không ghi địa chỉ giao hàng sẽ bị lỗi

--- Controllers/CartController.cs
+++ Controllers/CartController.cs
@@ -210,6 +210,12 @@ namespace Website_BanMayTinh.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Checkout(Order order, IFormFile? ReceiptImage, string PaymentMethod)
         {
+            if (!ModelState.IsValid)
+            {
+                // Gửi lại view cùng với model để hiển thị lỗi
+                return View(order);
+            }
+
             var cart = GetCart();
             if (cart == null || !cart.Any())
             {

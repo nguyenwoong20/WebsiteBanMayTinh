0bf5e9833956d054197cacba039961372bcfb773
Nguyen Le Hoan Hao <nguyenlehoanhao2004@gmail.com>
2025-06-27 13:06:47 +0700

    Update Migration_AddIdentity
    Fix Product->Name sang nvarchar(150) thay vì 100

--- Migrations/20250621140829_AddIdentity.cs
+++ Migrations/20250621140829_AddIdentity.cs
@@ -188,7 +188,7 @@ namespace Website_BanMayTinh.Migrations
                 {
                     Id = table.Column<int>(type: "int", nullable: false)
                         .Annotation("SqlServer:Identity", "1, 1"),
-                    Name = table.Column<string>(type: "nvarchar(100)", maxLength: 100, nullable: false),
+                    Name = table.Column<string>(type: "nvarchar(150)", maxLength: 150, nullable: false),
                     Price = table.Column<decimal>(type: "decimal(18,0)", nullable: false),
                     Description = table.Column<string>(type: "nvarchar(1500)", maxLength: 1500, nullable: true),
                     ImageUrl = table.Column<string>(type: "varchar(60)", maxLength: 60, nullable: true),
